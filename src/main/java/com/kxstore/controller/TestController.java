2e772a7a120224dda2d48b2312a5a9265db71089
xub2333 <miss.xu.bin@gmail.com>
2022-01-23 18:42:21 +0900

    init

---
+++ src/main/java/com/kxstore/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.kxstore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping("/hello1")
+	public String test() {
+		return "hello";
+	}
+}
---
+++ src/test/java/com/kxstore/KxstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kxstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KxstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
